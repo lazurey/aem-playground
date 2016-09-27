4896a940e773fc96df23d588a7e5b073b104d839
Sause <lazureys@gmail.com>
2016-09-27 08:34:21 +0800

    submit react demo for switching laptop

---
+++ content/src/main/content/jcr_root/apps/aem-playground/components/facebook/clientlibs/js/app.js
@@ -0,0 +1,14 @@
+// var HelloMessage = React.createClass({
+//   render: function() {
+//     return <div>Hello { this.props.name }</div>;
+//   }
+// });
+
+// var rootNode = document.getElementById('example');
+
+// ReactDOM.render(<HelloMessage name="John" />, rootNode);
+
+ReactDOM.render(
+  <h1>Hello, world!2</h1>,
+  document.getElementById('example')
+);
